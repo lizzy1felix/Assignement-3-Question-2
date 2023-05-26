7a3e7bc633160ca15fbdbf1765cd19a2f22e91af
lizzy1felix <134232794+lizzy1felix@users.noreply.github.com>
2023-05-26 16:32:41 +0100

    Create myfile.php

---
+++ myfile.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Info</title>
+</head>
+<body>
+
+<?php
+// Student Info
+$name = "FELIX ELIZABETH";
+$registrationNumber = "2021/HND/34124/CS";
+$stateOfOrigin = "Imo State";
+$projectTopic = " Design and Implementation of Smart Health Care System for Remote Monitoring of BP Patience";
+
+// Output the data 
+echo "<h1>Student Data</h1>";
+echo "<p><strong>Name:</strong> " . $name . "</p>";
+echo "<p><strong>Registration Number:</strong> " . $registrationNumber . "</p>";
+echo "<p><strong>State of Origin:</strong> " . $stateOfOrigin . "</p>";
+echo "<p><strong>Project Topic:</strong> " . $projectTopic . "</p>";
+?>
+
+</body>
+</html>
